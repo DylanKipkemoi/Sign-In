479ee3cad6d669236e7d6635695dedd953c7c8ae
Dylan <kipkemoiDylan254@gmail.com>
2021-02-13 01:15:17 +0300

    Sign-In

---
+++ js/main.js
@@ -0,0 +1,105 @@
+
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+
+})(jQuery);
+
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+     document.getElementById("sign_div").style.display = "block";
+     document.getElementById("login_div").style.display = "none";
+  } else {
+     document.getElementById("sign_div").style.display = "none";
+     document.getElementById("login_div").style.display = "block";
+    // No user is signed in.
+  }
+})
+
+function login(){
+
+    var userName = document.getElementById("email_field").value;
+    var password = document.getElementById("pass_field").value;
+
+
+
+
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+  .catch((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+
+
+    window.alert("Error " + errorMessage)
+  });
+}
